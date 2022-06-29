99a4bde365b70de35ee1e2c465fd4f246d01dd94
Carlos Gomez <cvgomez@gmail.com>
2022-06-29 10:00:01 -0400

    Update wowchemy-slides.js

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:C:\Users\cvgom\OneDrive\Analytical\Pagina_Web\website_2022\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5\assets\js\wowchemy-utils.js
+  // ns-hugo:C:\Users\Lenovo\OneDrive\Analytical\Pagina_Web\website_2022\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
